aa2009158ce7e2c9d72a881b075936deb04d7218
bilalyarmaci <92362207+bilalyarmaci@users.noreply.github.com>
2024-04-16 06:28:37 +0300

    add page routes and controllers, refactor customer (user) controllers

---
+++ server/controllers/pageController.js
@@ -0,0 +1,17 @@
+const homepage = async (req, res) => {
+    const locals = {
+        title: 'Home Page',
+        description: 'This is the home page'
+    }
+    res.render('index.ejs', locals);
+}
+
+const register = async (req, res) => {
+    const locals = {
+        title: 'Register',
+        description: 'This is the registration page'
+    }
+    res.render('register.ejs', locals);
+}
+
+export {homepage, register}
--- server/routes/customer.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const customerController = require("../controllers/customerController.js");
-
-const router = express.Router();
-
-router.get('/', customerController.homepage);
-
-module.exports = router;
