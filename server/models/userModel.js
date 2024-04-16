714fc2937adf5fa228527b7372bf81db5481ad6b
bilalyarmaci <92362207+bilalyarmaci@users.noreply.github.com>
2024-04-16 06:29:07 +0300

    create user model

---
+++ server/models/userModel.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    address: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
