732a029c69656626bea3a93997d6478514ecb87a
bilalyarmaci <92362207+bilalyarmaci@users.noreply.github.com>
2024-04-16 07:25:16 +0300

    add user registration functionality

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,7 +3,6 @@ import * as userController from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.route('/').get(userController.homepage);
-router.route('/register').get(userController.register);
+router.route('/').post(userController.registerUser);
 
 export default router;
