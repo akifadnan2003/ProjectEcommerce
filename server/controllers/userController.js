9c8a03d27312039b3c9f167d29f7a31488a5dfe7
bilalyarmaci <92362207+bilalyarmaci@users.noreply.github.com>
2024-06-06 13:40:32 +0300

    add getUser & logoutUser controllers & routers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,11 +2,6 @@ import User from '../models/userModel.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-// Function to hash password
-const hashPassword = async (password) => {
-    const salt = await bcrypt.genSalt(10);
-    return await bcrypt.hash(password, salt);
-}
 // Login a user
 const loginUser = async (req, res) => {
     try {
@@ -33,7 +28,7 @@ const loginUser = async (req, res) => {
 
         // Store the JWT in a cookie
         // CHANGE secure TO TRUE AND sameSite TO 'none' FOR PRODUCTION!!!
-        res.cookie('token', token, { httpOnly: true, secure: false, sameSite: 'lax' });
+        res.cookie('token', token, { httpOnly: true, secure: false, sameSite: 'lax', maxAge: 1000*60*60*1 });
 
         // Return a success response
         res.status(200).json({
@@ -70,4 +65,46 @@ const registerUser = async (req, res) => {
     }
 }
 
-export { loginUser, registerUser };
+// Get the user
+const getUser = (req, res) => {
+    try {
+        // Check if the user object exists on the request
+        if (!req.user) {
+            return res.status(400).json({
+                success: false,
+                error: 'User not found'
+            });
+        } else {
+            // Return the user object
+            res.status(200).json({
+                success: true,
+                data: req.user
+            });
+        }
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            error: error.message
+        });
+    }
+}
+
+const logoutUser = (req, res) => {
+    console.log('Logged out');
+    // CHANGE secure TO TRUE AND sameSite TO 'none' FOR PRODUCTION!!!
+    res.cookie('token', '', { httpOnly: true, secure: false, sameSite: 'lax', maxAge: 1 });
+    res.status(200).json({
+        success: true,
+    });
+}
+
+export { loginUser, logoutUser, registerUser, getUser };
+
+
+
+// Function to hash password
+const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+}
