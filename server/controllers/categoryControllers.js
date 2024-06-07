1222edf0c6e92d80a5ce94303cf49278085c5721
AnasYusuf14 <anas.yusuf200112@gmail.com>
2024-06-07 22:30:47 +0300

    chore: Refactor categoryControllers.js for error handling and middleware

--- server/controllers/categoryControllers.js
+++ server/controllers/categoryControllers.js
@@ -1,7 +1,7 @@
 import Category from '../models/categoryModel.js';
 import slugify from 'slugify';
 import asyncHandler from 'express-async-handler';
-import ApiError  from '../utils/apiError.js';   
+import ApiError from '../utils/apiError.js';
 //@desc Get all categories
 //@route GET /category
 //@access Public
@@ -11,51 +11,51 @@ export const getCategories = asyncHandler(async (req, res) => {
     const limit = parseInt(req.query.limit) || 12;
     const skip = (page - 1) * limit;
     const categories = await Category.find({}).skip(skip).limit(limit);
-    res.status(200).json({results : categories.length,page,data:categories});
+    res.status(200).json({ results: categories.length, page, data: categories });
 });
 
 //@desc Get a specific category by id
 //@route GET /category/:id
 //@access Public
-export const getCategory = asyncHandler(async (req, res ,next) => {
-    const {id} = req.params;
+export const getCategory = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
     const category = await Category.findById(id);
-    if(!category){
-       return next(new ApiError(`Category not found for this ID: ${id}`,404));
+    if (!category) {
+        return next(new ApiError(`Category not found for this ID: ${id}`, 404));
     }
-    res.status(200).json({data:category});
-}); 
+    res.status(200).json({ data: category });
+});
 
 //@desc Create a category
 //@route POST /category
 //@access Private
 export const createCategory = asyncHandler(async (req, res) => {
     const name = req.body.name;
-    const category = await Category.create({name,slug:slugify(name)});
-    res.status(201).json({data:category});
+    const category = await Category.create({ name, slug: slugify(name) });
+    res.status(201).json({ data: category });
 });
 
 //@desc Update a specific category
 //@route PUT /category/:id
 //@access Private
-export const updateCategory = asyncHandler(async (req, res,next) => {
-    const {id} = req.params;
-    const {name} = req.body;
-    const category = await Category.findOneAndUpdate({_id:id},{name , slug:slugify(name)},{new:true});
-    if(!category){
-        return next(new ApiError(`Category not found for this ID: ${id}`,404));
+export const updateCategory = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const category = await Category.findOneAndUpdate({ _id: id }, { name, slug: slugify(name) }, { new: true });
+    if (!category) {
+        return next(new ApiError(`Category not found for this ID: ${id}`, 404));
     }
-    res.status(200).json({data:category});
+    res.status(200).json({ data: category });
 });
 
 //@desc Delete a specific category
 //@route DELETE /category/:id
 //@access Private
-export const deleteCategory = asyncHandler(async (req, res ,next) => {
-    const {id} = req.params;
+export const deleteCategory = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
     const category = await Category.findByIdAndDelete(id);
-    if(!category){
-        return next(new ApiError(`Category not found for this ID: ${id}`,404));
+    if (!category) {
+        return next(new ApiError(`Category not found for this ID: ${id}`, 404));
     }
     res.status(204).send();
 });
